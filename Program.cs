25631883ae61f9a2a25adac5a4a01871c0c65f7b
Daniel <evdokimov.d.1308@gmail.com>
2025-01-20 18:06:38 +0300

    update

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ public class Employee
     }
 }
 
-
+// Класс для работы с данными
 public class Employees
 {
     public Employees(List<Employee> listEmployees)
@@ -31,39 +31,26 @@ public class Employees
     
     public Employee FindEmployeeById(string id)
     {
-        foreach (Employee person in _listEmployees)
-        {   
-            if (person.Id==id)
-                {
-                    return person;
-                } 
-        }
-        return null;
+
     }
     
     public void AddEmployee(Employee employee)
     {
-        _listEmployees.Add(employee);
+
     }
     
     public void UpdateEmployee(Employee employee)
     {
-        var employeeToUpdate = _listEmployees.FirstOrDefault(e => e.Id == employee.Id);
-        employeeToUpdate.Name = employee.Name;
-        employeeToUpdate.Department = employee.Department;
-        employeeToUpdate.Position = employee.Position;
+
     }
     
     public void RemoveEmployee(string id)
     {
-        var employeeToUpdate = _listEmployees.FirstOrDefault(e => e.Id == id);
-        
-        if (employeeToUpdate != null)
-        {
-            _listEmployees.Remove(employeeToUpdate);
-        }
+
     }
 }
+
+
 
 public class Program
 {   
@@ -72,7 +59,7 @@ public class Program
         Employee employee1 = new Employee("Сьюзан Майерс","47899","Бухгалтерия","Вице-президент");
         Employee employee2 = new Employee("Боб","12345","Офис","Работник");
 
-        var employees = new List<Employee> {employee1, employee2};
+        List<Employee> employees = new List<Employee> {employee1, employee2};
 
         Console.WriteLine("1. Найти сотрудника по идентификационному номеру");
         Console.WriteLine("2. Добавить нового сотрудника");
@@ -89,74 +76,13 @@ var emploeesList = new Employees(employees);
             switch (choice)
             {
                 case "1":
-                    Console.WriteLine("Введите идентификационный номер сотрудника");
-                    var id = Console.ReadLine();
-                    var employee = emploeesList.FindEmployeeById(id);
-                    if (employee != null)
-                    {
-                        Console.WriteLine("{0}\t{1}\t{2}\t{3}", employee.Name, employee1.Id, employee.Department, employee.Position);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Сотрудник не найден");
-                    }
-                    break;
+
                 case "2":
-                    Console.WriteLine("Введите идентификационный номер сотрудника");
-                    var idNewEmployee = Console.ReadLine();
-                    Console.WriteLine("Введите имя сотрудника");
-                    var nameNewEmplyee = Console.ReadLine();
-                    Console.WriteLine("Введите отдел сотрудника");
-                    var departmentNewEmplyee = Console.ReadLine();
-                    Console.WriteLine("Введите должность сотрудника");
-                    var positionNewEmplyee = Console.ReadLine();
-                    
-                    var newEmployee = new Employee(nameNewEmplyee, idNewEmployee, departmentNewEmplyee, positionNewEmplyee);
-                    emploeesList.AddEmployee(newEmployee);
-                    Console.WriteLine("Сотрудник успешно добавлен");
-                    break;
+
                 case "3":
-                    Console.WriteLine("Введите идентификационный номер сотрудника");
-                    var idUpdateEmployee = Console.ReadLine();
-                    
-                    var employeeUpdate = emploeesList.FindEmployeeById(idUpdateEmployee);
-                    
-                    if(employeeUpdate != null)
-                    {
-                        Console.WriteLine("Введите имя сотрудника");
-                        var nameUpdateEmplyee = Console.ReadLine();
-                        Console.WriteLine("Введите отдел сотрудника");
-                        var departmentUpdateEmplyee = Console.ReadLine();
-                        Console.WriteLine("Введите должность сотрудника");
-                        var positionUpdateEmplyee = Console.ReadLine();
-                        
-                        employeeUpdate.Name = nameUpdateEmplyee;
-                        employeeUpdate.Department = departmentUpdateEmplyee;
-                        employeeUpdate.Position = positionUpdateEmplyee;
-                        
-                        emploeesList.UpdateEmployee(employeeUpdate);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Сотрудник не найден");
-                    }
-                    break;
+
                 case "4":
-                    Console.WriteLine("Введите идентификационный номер сотрудника");
-                    var idRemoveEmployee = Console.ReadLine();
-                    
-                    var employeeRemove = emploeesList.FindEmployeeById(idRemoveEmployee);
-                    
-                    if(employeeRemove != null)
-                    {
-                        emploeesList.RemoveEmployee(idRemoveEmployee);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Сотрудник не найден");
-                    }
-                    
-                    break;
+
                 case "0":
                     flag = false;
                     break;
