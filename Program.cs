37b8dc7a8e3e50a5a3810b4f9eeb54156af9343c
Daniel <evdokimov.d.1308@gmail.com>
2025-01-24 19:39:38 +0300

    console_app

--- Program.cs
+++ Program.cs
@@ -3,54 +3,101 @@ using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
 using System.Security.AccessControl;
 
+public class Employee
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Department { get; set; }
+    public string Position { get; set; }
 
-
-//Основной класс
-    // Абстрактный класс
-    abstract class Animal
+    public Employee(string name, string id, string department, string position)
     {
-        public string Name;
-
-        public Animal(string name)
-        {
-            Name = name;
-        }
-
-        // Абстрактный метод (не имеет реализации)
-        public abstract void Speak();
+        Id = id;
+        Name = name;
+        Department = department;
+        Position = position;
     }
+}
 
-    // Класс-наследник от абстрактного класса
-    class Cat : Animal
+
+public class Employees
+{
+    private List<Employee> _listEmployees;
+    
+    public Employees(List<Employee> listEmployees)
     {
-        public string Breed;
-
-        public Cat(string name, string breed) : base(name)
-        {
-            Breed = breed;
-        }
-
-        // Реализация абстрактного метода
-        public override void Speak()
-        {
-            Console.WriteLine($"{Name} says Meow!");
-        }
+        _listEmployees = listEmployees;
     }
-
-
+    
+    //public Employee FindEmployeeById(string id)
+    public void FindEmployeeById(string id)
+    {
+ 
+    }
+    
+    public void AddEmployee(Employee employee)
+    {
+       
+    }
+    
+    public void UpdateEmployee(Employee employee)
+    {
+       
+    }
+    
+    public void RemoveEmployee(string id)
+    {
+      
+    }
+}
 
 public class Program
 {   
     public static void Main()
     {    
-        //Создаем новые объекты класса
-        Cat cat1 = new Cat("Бобик","дворняга");
-        cat1.Speak();
+        Employee employee1 = new Employee("Сьюзан Майерс","47899","Бухгалтерия","Вице-президент");
+        Employee employee2 = new Employee("Боб","12345","Офис","Работник");
+        Employee employee3 = new Employee("Джон","54321","Охрана","Охранник");
 
+        var employees = new List<Employee> {employee1, employee2};
+        employees.Add(employee3);
+
+        Console.WriteLine("1. Найти сотрудника по идентификационному номеру");
+        Console.WriteLine("2. Добавить нового сотрудника");
+        Console.WriteLine("3. Изменить имя, отдел и должность существующего сотрудника");
+        Console.WriteLine("4. Удалить сотрудника");
+        Console.WriteLine("0. Выйти из программы");
+
+        var emploeesList = new Employees(employees);
+        var flag = true;
+        while (flag)
+        {
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                   
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                    
+                    break;
+                case "4":
+                    
+                    break;
+                case "0":
+                    flag = false;
+                    break;
+                default:
+                    Console.WriteLine("Неверное значение");
+                    break;
+            
+            }
+        }
     }
 }
-
-
 
 
 
--- obj/Debug/net9.0/task1.AssemblyInfo.cs
+++ obj/Debug/net9.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25631883ae61f9a2a25adac5a4a01871c0c65f7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26edcb2ca81139752c3e65a2800ef5f0f62e0e4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
